3e3e2434eca7a1785d9306d1affa53a588e50d1c
HugDi <113516550+GugDi@users.noreply.github.com>
2023-10-09 20:34:47 +0400

    main.cpp - создание объекта класса

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <QApplication>
 #include "mainwindow.h"
 
-int main(int argc, char* argv[]) {};
+int main(int argc, char* argv[]) {
+  QApplication app(argc, argv);
+	MainWindow mainWin;
+	mainWin.show();
+	return app.exec();
+};
