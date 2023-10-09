200b4c26b9e71c043a2a0b5c6f9e421682dfed43
HugDi <113516550+GugDi@users.noreply.github.com>
2023-10-09 20:44:29 +0400

    mainwindow.cpp - MainWindow::MainWindow - размер "таймера"

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,10 @@
 #include "mainwindow.h"
 #include <QHBoxLayout>
 
-MainWindow::MainWindow(QWidget* parent)	: QMainWindow(parent), timeCount(0){}
+MainWindow::MainWindow(QWidget* parent)	: QMainWindow(parent), timeCount(0){
+	setFixedSize(200, 100);
+	setWindowTitle("Timer");
+}
 
 void MainWindow::startTimer(){
 	timer->start(1000); // Start the timer with 1 second interval
